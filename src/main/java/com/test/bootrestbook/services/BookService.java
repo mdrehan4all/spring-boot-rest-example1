45987e635bb312f1751d938a32d9ba5865c41ef2
Md Rehan <mdrehan4all@gmail.com>
2026-01-04 10:23:26 +0530

    Removed unused code

--- src/main/java/com/test/bootrestbook/services/BookService.java
+++ src/main/java/com/test/bootrestbook/services/BookService.java
@@ -4,21 +4,10 @@ import com.test.bootrestbook.dao.BookRepository;
 import com.test.bootrestbook.entities.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class BookService {
-//    private static List<Book> list = new ArrayList<>();
-//
-//    static{
-//        list.add(new Book(1, "ABC", "ABC"));
-//        list.add(new Book(2, "DEF", "ABC"));
-//        list.add(new Book(3, "XYZ", "ABC"));
-//    }
-
     @Autowired
     public BookRepository bookRepository;
 
@@ -30,32 +19,23 @@ public class BookService {
     public Book getBookById(int id){
         Book book = null;
         try {
-            //book = list.stream().filter(e -> e.getId() == id).findFirst().get();
             book = this.bookRepository.findById(id);
-        }catch(Exception e){}
+        }catch(Exception e){
+            e.getStackTrace();
+        }
         return book;
     }
 
     public Book addBook(Book book){
         return bookRepository.save(book);
-        //list.add(b);
     }
 
     public void deleteBook(int id){
-        //list = list.stream().filter(book->book.id != id).collect(Collectors.toList());
-        //return list;
         this.bookRepository.deleteById(id);
 
     }
 
     public void updateBook(Book book, int id){
-//        list = list.stream().map(b->{
-//            if(b.getId() == id){
-//                b.setName(book.getName());
-//                b.setAuthor(book.getAuthor());
-//            }
-//            return b;
-//        }).collect(Collectors.toList());
         book.setId(id);
         this.bookRepository.save(book);
     }
